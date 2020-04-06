5b241572ba5cac36e3f5b615c8c4e14ed346c3ff
Sung-Woo <seastory624@gmail.com>
2020-04-06 22:30:40 +0900

    2020-04-04

--- core/models.py
+++ core/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class TimeStampedModel(models.Model):
 
-    """Time Stamped Model"""
+    """ Time Stamped Model """
 
-    created = models.DataTimeField()
-    updated = models.DataTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstarct = True
-
+        abstract = True
--- rooms/models.py
+++ rooms/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.countries.fields import CountryField
+from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
 
 
 class Room(core_models.TimeStampedModel):
 
     """Room Model Definition"""
 
-    name = models.CharField(max_length=140, blank=True)
+    name = models.CharField(max_length=140, help_text="최대 140자 내로 입력가능합니다.")
     description = models.TextField()
     country = CountryField()
     city = models.CharField(max_length=80)
@@ -20,3 +21,4 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
