b96763bfceb8f91a7f6cbc59050b5a9f932306e8
sung-woo-jang <54757435+sung-woo-jang@users.noreply.github.com>
2020-04-14 23:54:15 +0900

    ~6.2

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,62 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        ("Basic Info", {"fields": ("name", "description", "country", "price")}),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book"),},),
+        (
+            "Spaces",
+            {
+                "classes": ("collapse",),
+                "fields": ("guests", "beds", "bedrooms", "baths"),
+            },
+        ),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rule")},
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+    # 따로 또 알아보기
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rule",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.conut()
+
+    # count_amenities.short_description = "hello sexy!"
 
 
 @admin.register(models.Photo)
