e19ba639ffc3588388a336a1bd04c932ac9f3176
seastory624@gmail.com <54757435+sung-woo-jang@users.noreply.github.com>
2020-05-08 14:43:22 +0900

    ~3.6

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-# asd
+
 @admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+"""     같은거임
+admon 패널에서 이 User를 보고싶다
+user를 컨트롤한 클래스가 바로 이거야
 class CustomUserAdmin(admin.ModelAdmin):
     pass
+
+admin.site.register(models.User, CustomUserAdmin) 
+"""
