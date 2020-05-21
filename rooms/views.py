8c999c494ff0846a95142f360f15125cadbc77f9
Sung-Woo <seastory624@gmail.com>
2020-05-22 08:49:16 +0900

    2020-05-21

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,5 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    all_rooms = models.Room.objects.all()[:5]
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
